41bf617ed00220dc3598542638186a8dc17c2177
MisterOz93 <70535624+MisterOz93@users.noreply.github.com>
2021-03-23 02:00:40 -0400

    Add files via upload
    delete works on one children nodes.

--- binary_tree.rb
+++ binary_tree.rb
@@ -1,11 +1,11 @@
 
 module Comparison
     def compare(x,y)
-        x > y ? true : false  #make x node.data, and y other_node.data 
+        x > y ? 1 : 0  
     end 
 end
 
-
+#delete works on leaf. continue on line 119 using parent method to point.
 
 class Node
     include Comparison
@@ -31,6 +31,21 @@ class Node
     def set_right_child(value)
         @right_child = value
     end
+    def number_of_children
+        output = 0
+        output += 1 if @left_child
+        output += 1 if @right_child
+        output
+    end
+    def delete
+        @data = nil
+        @left_child = nil
+        @right_child = nil
+    end
+    def parent(node = nil)
+        @parent = node if node
+        @parent
+    end
     def to_s    
         "   data:#{@data} \n lc:#{@left_child.read if @left_child}  rc: #{@right_child.read if @right_child}"
     end
@@ -50,7 +65,7 @@ class Tree < Node
 
         arr.sort!.uniq!
         if arr.length == 1 
-            #puts node.to_s
+           # puts node.to_s
             return
         end 
         
@@ -61,40 +76,70 @@ class Tree < Node
         end 
         left_array = arr[0...mid] 
         right_array = arr[mid + 1...arr.length] if arr[mid + 1] 
-        node.set_left_child(Node.new(left_array[(left_array.length / 2)]))
-        node.set_right_child(Node.new(right_array[(right_array.length / 2)])) if right_array
+        lc = Node.new(left_array[(left_array.length / 2)])
+        rc = Node.new(right_array[(right_array.length / 2)]) if right_array
+        lc.parent(node)
+        rc.parent(node) if rc
+        node.set_left_child(lc)
+        node.set_right_child(rc) if rc
         build_tree(left_array, node.left_child)
         build_tree(right_array, node.right_child) if right_array
         
         
-       # puts node.to_s 
+       #puts node.to_s 
         node
     end
 
-    def insert(value)
-        @arr << value
-        @arr.sort!.uniq!
-        self.build_tree
-    end
-
-    def delete(value)
-        if @arr.include?(value)
-            @arr -= [value]
-            self.build_tree
-        else
-            puts "The tree does not contain #{value}"
+    def insert(value, existing_node = @base_root)
+        if @arr.include? value
+            puts "the tree already has a node with #{value}."
+            return
         end
+       if value > existing_node.read && existing_node.right_child.nil?
+            existing_node.set_right_child(Node.new(value))
+       elsif value > existing_node.read && existing_node.right_child
+            self.insert(value,existing_node.right_child)
+       elsif value < existing_node.read && existing_node.left_child.nil?
+            existing_node.set_left_child(Node.new(value))
+       elsif value < existing_node.read && existing_node.left_child
+            self.insert(value, existing_node.left_child)
+       end
+       #puts existing_node
     end
 
-    def find(value, root = self.build_tree)
-        if root.read == value 
-            
-            puts "#{root} contains #{value}"
-            return root
-        elsif root.read > value
-            self.find(value, root.left_child)
-        else
-            self.find(value, root.right_child)
+    def delete(value) 
+        node = self.find(value)
+        unless node.is_a? Node
+            return
+        end
+       
+       if node.number_of_children == 0
+            node.delete
+            return
+       elsif node.number_of_children == 1 && node.left_child
+            #i need the node's left child to take its place.
+            node.parent.set_left_child(node.left_child)
+            node.delete
+          
+       elsif node.number_of_children == 1 && node.right_child
+            node.parent.set_right_child(node.right_child)
+            node.delete    
+       else #node has two children
+       end
+    end
+
+    def find(value, node = @base_root)
+        unless node.is_a? Node
+            puts "Could not find #{value}"
+            return
+        end
+    
+        if node.read == value 
+            return node
+        elsif node.read > value
+            self.find(value, node.left_child)
+        elsif node.read < value
+            self.find(value, node.right_child)
         end
         
 
@@ -102,7 +147,9 @@ class Tree < Node
 
     def level_order(root = @base_root) 
         #should return [6,3,9,2,5,8,10,1,4,7].
-        return if root.nil? 
+        if data.read.nil?
+            return
+        end
         @queue = [root] unless @queue
         @bfs = [] unless @bfs
         unless @queue.empty?
@@ -117,25 +164,127 @@ class Tree < Node
 
     end
 
-    def inorder
+    def inorder(data = @base_root) #left -> root -> right
+        if data.read.nil?
+            return
+        end
+        @inorder_list = [] unless @inorder_list
+        if data.left_child
+            inorder(data.left_child)
+        end
+        @inorder_list << data.read
+        if data.right_child
+            inorder(data.right_child)
+        end
+        @inorder_list
+
     end
 
-    def preorder
+    def preorder(data = @base_root) # root -> left -> right
+        if data.read.nil?
+            return
+        end
+        @preorder_list = [] unless @preorder_list
+        @preorder_list << data.read
+        if data.left_child
+            preorder(data.left_child)
+        end
+        if data.right_child
+            preorder(data.right_child)
+        end
+        @preorder_list
     end
 
-    def postorder
+    def postorder(data = @base_root) # left -> right -> root
+        if data.read.nil?
+            return
+        end
+        @postorder_list = [] unless @postorder_list
+        if data.left_child
+            postorder(data.left_child)
+        end
+        if data.right_child
+            postorder(data.right_child)
+        end
+        @postorder_list << data.read
+        @postorder_list
+
     end
 
-    def height
+    def height(node_value) #node to furthest leaf
+        node = self.find(node_value)    
+        @height = 0 unless @height
+        @height = 0 if @reset == 1
+        unless node.left_child || node.right_child
+            @reset = 0
+            return @height if @height == 0
+        end
+        if node.left_child && node.right_child.nil?
+            @reset = 0
+            @height += 1
+            self.height(node.left_child.read) 
+        end
+        if node.right_child && node.left_child.nil?
+            @reset = 0
+            @height += 1
+            self.height(node.right_child.read)
+        end
+        if node.right_child && node.left_child
+            @reset = 0
+            @height += 1
+            rc = node.right_child
+            lc = node.left_child
+            if lc.number_of_children > rc.number_of_children
+                self.height(lc.read)
+            else
+                self.height(rc.read)
+            end
+            
+        end
+        @reset = 1
+        @height
     end
 
-    def depth
+    def depth(node_value) #node to root
+        @depth = 0 
+        target_node = self.find(node_value)
+        starting_node = @base_root
+        until starting_node == target_node
+            if starting_node.read > target_node.read
+                @depth += 1
+                starting_node = starting_node.left_child
+            elsif
+                starting_node.read < target_node.read
+                @depth += 1
+                starting_node = starting_node.right_child
+            end
+        end
+        @depth
+        
     end
 
-    def balanced?
+    def balanced?(node = @base_root)#difference between heights of left subtree and 
+                #right subtree of every node is not more than 1.
+    if node.left_child
+        left_height = self.height(node.left_child.read) 
+    else 
+        left_height = 0 
     end
+    if node.right_child
+     right_height = self.height(node.right_child.read) 
+    else
+        right_height = 0 
+    end
+     if right_height - left_height > 1 || right_height - left_height < 0
+        return nil
+     else 
+        return true
+    end
+    balanced?(node.left_child) if node.left_child
+    balanced?(node.right_child) if node.right_child
+end
 
-    def rebalance
+    def rebalance #rebalance if the tree is unbalanced. read every node into a new array, then build new tree
     end
 
 
@@ -143,4 +292,15 @@ end
 
 tree = Tree.new([1,2,3,4,5,6,7,8,9,10])
 tree.build_tree
-print tree.level_order
+#print tree.find(3)
+tree.delete(2)
+print tree.find(3)  #should return data: 3, lc: 1, rc: 5 after i properly delete 2
+#print tree.inorder
+#print tree.postorder
+#print tree.preorder
+#print tree.level_order
+#print tree.height(6)
+#print tree.height(9)
+#print tree.depth(1)
+#print tree.balanced?
+#print tree.find(10)
